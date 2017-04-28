ad408fca5e93d04c74bcf9d027cd900932516da7
admin <admin>
2017-04-28 09:48:17 +0800

    newsest

---
+++ js/public.js
@@ -0,0 +1,16 @@
+//移动端适配问题
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = (clientWidth / 6.4) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false)
+})(document, window);
+window.onload=function(){
+
+}
